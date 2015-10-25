2ccc09eedf3ccea538085639b5d10791bf3f9f94
Shawn <shawn@weecology.org>
2015-10-25 14:59:22 -0400

    added nightly low temp

--- weatherDependentGrowthModel.R
+++ weatherDependentGrowthModel.R
@@ -4,22 +4,27 @@ rodents=read.csv(paste(dataFolder, 'RodentsAsOfSep2015.csv', sep=''), na.strings
 sppCodes=read.csv(paste(dataFolder, 'PortalMammals_species.csv', sep=''))
 
 
-#####################################################
+###################################################################################################
 #Functions to retrieve weather data
+##################################################################################################
 precipRaw=read.csv(paste(dataFolder,'Hourly_PPT_mm_1989_present_fixed.csv',sep=''))
 
+
+#Setup a weather dataframe to work with nightly temp and precip data. 
 #Days in the capture data are always from the morning of trapping. So here I need to sum precip from the prior
-#night. Mean sunset/sunrise times throughout the year are roughly 6pm-6am
+#night, or get the temp info across two different days. Mean sunset/sunrise times throughout the year are roughly 6pm-6am
 
 uniqueDays=unique(precipRaw[,c('Year','Month','Day')])
 #Explicetly order by year, month, and day
 uniqueDays=uniqueDays[with(uniqueDays, order(Year,Month,Day)),]
 #Extract hourly readings for the evening hours 6pm-6am
 nightlyPrecip=subset(precipRaw, (Hour>=1800 & Hour <=2400) | (Hour>=100 & Hour<=600))
-nightlyPrecip=nightlyPrecip[,c('Year','Month','Day','Hour','Precipitation')]
 #Label hours evening/morning. 
 nightlyPrecip$TimeOfDay='morning'
 nightlyPrecip$TimeOfDay[nightlyPrecip$Hour>1200]='evening'
+
+####################################################
+#Get the total precip from a previous night of trapping
 
 getNightlyPrecip=function(year,month,day){
   #From the day given, get the previous days actual date from the unique list. This will account for nights across
@@ -38,3 +43,27 @@ getNightlyPrecip=function(year,month,day){
   
   return(morningPrecip+eveningPrecip)
 }
+
+#####################################################
+#Get the low temp from a previous night of trapping
+
+#Gives nightly low
+getNightlyTemp=function(year, month, day){
+  todayIndex=which(uniqueDays$Year==year & uniqueDays$Month==month & uniqueDays$Day==day)
+  yesterdayIndex=todayIndex-1
+  yesterdayYear=uniqueDays$Year[yesterdayIndex]
+  yesterdayMonth=uniqueDays$Month[yesterdayIndex]
+  yesterdayDay=uniqueDays$Day[yesterdayIndex]
+  
+  morningTemp=subset(nightlyPrecip, Year==year & Month==month & Day==day & TimeOfDay=='morning')
+  morningTemp=min(morningTemp$TempAir)
+  
+  eveningTemp=subset(nightlyPrecip, Year==yesterdayYear & Month==yesterdayMonth & Day==yesterdayDay & TimeOfDay=='evening')
+  eveningTemp=min(eveningTemp$TempAir)
+  
+  return(min(morningTemp, eveningTemp))
+}
+
+
+
+
